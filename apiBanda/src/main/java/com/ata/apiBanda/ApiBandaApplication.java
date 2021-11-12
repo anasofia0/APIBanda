677f07bbe2a0813139693f536306c018d5585d52
anasofia0 <anasofialaranja@gmail.com>
2021-11-12 08:49:15 -0300

    começo projeto spring

---
+++ apiBanda/src/main/java/com/ata/apiBanda/ApiBandaApplication.java
@@ -0,0 +1,13 @@
+package com.ata.apiBanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiBandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiBandaApplication.class, args);
+	}
+
+}
